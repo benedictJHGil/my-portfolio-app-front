4e4f7a0411bdbfd89644ef892bcb1a8ec85e1245
benedictJHGil <uniquehan79@gmail.com>
2025-11-13 16:41:18 +0900

    Modify contact modal on the about page

--- src/components/ContactModal/ContactModal.tsx
+++ src/components/ContactModal/ContactModal.tsx
@@ -33,7 +33,7 @@ function ContactModal({ isOpen, onClose }: ContactData) {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         console.log("문의 내용:", form);
-        alert("문의가 전송되었습니다!");
+        alert("현재 개발중에 있습니다.");
         setForm({ title: "", type: "", contact: "", message: "" });
         onClose();
     };
@@ -54,6 +54,7 @@ function ContactModal({ isOpen, onClose }: ContactData) {
                     </button>
                 </div>
                 <hr />
+                <h2 style={{ color: 'red', fontSize: '25px', textAlign: 'center' }}>현재 개발중에 있습니다.</h2>
                 <form onSubmit={handleSubmit} className="contact-form">
                     <div className="form-row">
                         <label htmlFor="title" className="form-label">제목</label>
