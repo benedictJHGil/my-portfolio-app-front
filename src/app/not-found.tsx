3314a2a3ebc518ad728a102f6ff96e50e6341820
benedictJHGil <uniquehan79@gmail.com>
2025-11-18 15:08:59 +0900

    Add NotFonud page and Modify button

---
+++ src/app/not-found.tsx
@@ -0,0 +1,43 @@
+import "./not-found.page.css";
+import MyImage from "@/components/MyImage/MyImage";
+import Button from "@/components/Button/Button";
+
+function NotFound() {
+  return (
+    <div className="container">
+        <MyImage
+            src={"/images/404_page.png"}
+            alt={"404"}
+            width={300} 
+            height={300}
+            className={"image-404"}
+        />
+
+        <h1 className="title">페이지를 찾을 수 없습니다.</h1>
+
+        <p className="description">
+            주소가 잘못되었거나, 더 이상 존재하지 않는 페이지입니다.<br />
+            아래 버튼을 이용해 이동해 주세요.
+        </p>
+
+        <div className="actions">
+            <Button
+                href={"/"}
+                className={"button link-btn-404 link-home"}
+            >
+                홈으로 가기
+            </Button>
+            {/* <Link href="/" className="link-home">홈으로 가기</Link> */}
+            <Button
+                href={"/"}
+                className={"button link-btn-404 link-main"}
+            >
+                포트폴리오 보러 가기
+            </Button>
+            {/* <Link href="/portfolio/main" className="link-main">포트폴리오 보러 가기</Link> */}
+        </div>
+    </div>
+  )
+}
+
+export default NotFound
