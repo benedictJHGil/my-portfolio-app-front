64ccf7c8b7a0ee3966db0492e83cb4b6f6e67e47
benedictJHGil <uniquehan79@gmail.com>
2025-11-25 21:20:53 +0900

    Modify import error on the about page

--- src/components/ProfileDetails/ProfileDetails.tsx
+++ src/components/ProfileDetails/ProfileDetails.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from "react";
-import Image from "next/image"
 import "./ProfileDetails.page.css"
 import Button from "../Button";
 import ContactModal from "../ContactModal/ContactModal";
